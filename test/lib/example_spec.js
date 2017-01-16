8c5264e5175c62575945be962fb009acd45a8fe1
Paulo Diovani <paulo@diovani.com>
2017-01-16 14:38:04 -0200

    Spelling fixes

--- test/lib/example_spec.js
+++ test/lib/example_spec.js
@@ -4,14 +4,14 @@ const Example = require('../../lib/example');
 
 describe('Example', () => {
 
-    describe('#isMissingVariables', () => {
+    describe('#hasMissingVariables', () => {
 
         context('when .env.example does not exists', () => {
 
             it('should throw an exception', (done) => {
 
                 try {
-                    Example.isMissingVariables(".env.sample");
+                    Example.hasMissingVariables(".env.sample");
                 } catch(e) {
                     expect(e.code).to.equal("ENOENT");
                 }
